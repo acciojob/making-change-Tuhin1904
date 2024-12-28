ae2ea520a4e1c7ffc6cd2b006623ee5f1af29d76
Tuhin Ghosh <66065760+Tuhin1904@users.noreply.github.com>
2024-12-28 14:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const makeChange = (c) => {
   // your name here
-};
+	const change  = {
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
+	const coinValue = {
+		q:25,
+		d:10,
+		n:5,
+		p:1
+	}
 
+	for(let coin in coinValue){
+		// console.log("Coin:",coin)
+		change[coin] = Math.floor(c/coinValue[coin]);
+		c = c%coinValue[coin]
+	}
+
+	return change;
+};
+// makeChange(134)
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
